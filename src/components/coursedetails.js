fb413c5f7d49615065a7443600594e76fe6c4976
Kiran <chandamalakiran2@gmail.com>
2025-05-12 16:18:46 +0530

    final commit

--- src/components/coursedetails.js
+++ src/components/coursedetails.js
@@ -13,7 +13,7 @@ export default function CourseDetails() {
         
         className='course-details'> 
             <Link to="/courses"><button> <i style={{color:"white"}} className="fa-solid fa-arrow-left"></i> Back to Courses</button></Link>
-            <img src={course.image}/>
+            <img src={course.image} alt='img'/>
             <h1>{course.title}</h1>
             <p>{course.description}</p>
             <h2>Course Details</h2>
